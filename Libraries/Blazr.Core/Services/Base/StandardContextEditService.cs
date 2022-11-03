e1a04b10972032aeb4191e40f876f96614b4c36f
Shaun Curtis <shaun@coldelm.com>
2022-11-03 09:12:49 +0000

    Update

---
+++ Libraries/Blazr.Core/Services/Base/StandardContextEditService.cs
@@ -0,0 +1,146 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+namespace Blazr.Core;
+
+public class StandardEditContextService<TRecord, TEditRecord, TEntity>
+    : BaseViewService<TRecord, TEntity>, IEditService<TRecord, TEditRecord, TEntity>
+    where TRecord : class, new()
+    where TEditRecord : class, IEditRecord<TRecord>, new()
+    where TEntity : class, IEntity
+{
+    public TEditRecord EditModel { get; private set; } = new TEditRecord();
+
+    public StandardEditContextService(ICQSDataBroker dataBroker, INotificationService<TEntity> notifier, AuthenticationStateProvider authenticationState, IAuthorizationService authorizationService)
+        : base(dataBroker, notifier, authenticationState, authorizationService)
+    { }
+
+    public async ValueTask<bool> LoadRecordAsync(Guid Id)
+    {
+        if (Id == Guid.Empty)
+        {
+            await GetNewEditRecordAsync(null);
+            return true;
+        }
+
+        var result = await this.GetRecordAsync(Id);
+
+        var haveAuthorizedRecord = result.Success
+            && result.Record is not null
+            && await this.CheckRecordAuthorization(result.Record, this.ReadPolicy);
+
+        this.Message = result.Success && !haveAuthorizedRecord
+            ? "You are not authorized to access the record"
+            : this.Message;
+
+        var record = haveAuthorizedRecord
+            ? result.Record!
+            : new TRecord();
+
+        this.EditModel.Load(record);
+
+        return haveAuthorizedRecord;
+    }
+
+    public ValueTask<bool> GetNewEditRecordAsync(TRecord? record)
+    {
+        this.EditModel.Load(record ?? new TRecord());
+        return ValueTask.FromResult(true);
+    }
+
+    public async ValueTask<bool> AddRecordAsync()
+    {
+        this.Message = string.Empty;
+
+        if (!await this.CheckAuthorization(this.AddPolicy))
+        {
+            this.Message = "You are not authorized to add a record";
+            return false;
+        }
+
+        if (!EditModel.IsNew)
+        {
+            this.Message = "You can't add an existing record";
+            return false;
+        }
+
+        var record = EditModel.AsNewRecord;
+        var command = AddRecordCommand<TRecord>.GetCommand(record);
+        var result = await this.DataBroker.ExecuteAsync<TRecord>(command);
+
+        if (result.Success)
+        {
+            this.EditModel.Load(record);
+            this.NotifyChange(EditModel.Uid);
+        }
+
+        this.Message = result.Success
+            ? String.Empty
+            : "Failed to add the record";
+
+        return result.Success;
+    }
+
+    public async ValueTask<bool> UpdateRecordAsync()
+    {
+        var record = EditModel.Record;
+
+        if (!await this.CheckRecordAuthorization(this.EditModel.CleanRecord, this.EditPolicy))
+            return false;
+
+        var command = UpdateRecordCommand<TRecord>.GetCommand(record);
+        var result = await this.DataBroker.ExecuteAsync<TRecord>(command);
+
+        if (result.Success)
+        {
+            this.EditModel.Load(record);
+            this.NotifyChange(EditModel.Uid);
+        }
+
+        this.Message = result.Success
+            ? String.Empty
+            : "Failed to update the record";
+
+        return result.Success;
+    }
+
+    public async ValueTask<bool> DeleteRecordAsync()
+    {
+        var record = EditModel.CleanRecord;
+        var id = EditModel.Uid;
+
+        if (id == Guid.Empty)
+        {
+            this.Message = "No record to delete";
+            return false;
+        }
+
+        if (!await this.CheckRecordAuthorization(record, this.DeletePolicy))
+            return false;
+
+        var command = DeleteRecordCommand<TRecord>.GetCommand(record);
+        var result = await this.DataBroker.ExecuteAsync<TRecord>(command);
+
+        if (result.Success)
+        {
+            this.EditModel.Load(new TRecord());
+            this.NotifyChange(id);
+        }
+
+        this.Message = result.Success
+            ? string.Empty
+            : result.Message;
+
+        return result.Success;
+    }
+
+    private void NotifyChange(Guid? Uid = null)
+    {
+        if (Uid is not null)
+            this.Notifier?.NotifyRecordChanged(this, Uid ?? Guid.Empty);
+
+        this.Notifier?.NotifyListUpdated(this);
+    }
+}
---
+++ Libraries/Blazr.UI/Components/Forms/Blazr_Editor_Form.cs
@@ -0,0 +1,178 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+
+namespace Blazr.UI;
+
+public abstract partial class Blazr_Editor_Form<TEditContext>
+    :UITemplatedComponentBase
+    where TEditContext : class, IRecordEditContext, new()
+{
+    protected EditContext editContext = default!;
+    protected string? alertMessage;
+    protected string alertColour = "alert-info";
+    protected int alertTimeOut = 0;
+    protected Guid alertId;
+    protected bool isConfirmDelete = false;
+
+    public override async Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.SetParameterProperties(this);
+
+        await PreLoadRecordAsync();
+
+        if (!isNew)
+        {
+            await base.SetParametersAsync(ParameterView.Empty);
+            return;
+        }
+
+        if (!string.IsNullOrWhiteSpace(this.EntityUIService.SingleTitle))
+            this.FormTitle = $"{this.EntityUIService.SingleTitle} Editor";
+
+        this.Service.SetServices(this.SPAServiceProvider);
+
+        this.LoadState = ComponentState.Loading;
+
+        _ = Id != Guid.Empty
+            ? await this.Service.LoadRecordAsync(Id)
+            : await this.Service.GetNewEditRecordAsync(await GetNewRecord());
+
+        this.editContext = new EditContext(this.Service.EditModel);
+        this.editContext.OnFieldChanged += FieldChanged;
+
+        if (this.blazrNavManager is not null)
+        {
+            this.blazrNavManager.BrowserExitAttempted += FailedExitAttempt;
+            this.blazrNavManager.NavigationEventBlocked += FailedRoutingAttempt;
+        }
+
+        this.LoadState = ComponentState.Loaded;
+        await base.SetParametersAsync(ParameterView.Empty);
+        isNew = false;
+    }
+
+    protected BlazrNavigationManager? blazrNavManager
+    => NavManager is BlazrNavigationManager ? NavManager as BlazrNavigationManager : null;
+
+    protected bool IsDirty
+        => this.Service.EditModel.IsDirty;
+
+    protected bool IsNew
+        => this.Service.EditModel.IsNew;
+
+    protected virtual Task<TRecord> GetNewRecord()
+        => Task.FromResult(new TRecord());
+
+    private void FieldChanged(object? sender, FieldChangedEventArgs e)
+    {
+        this.blazrNavManager?.SetLockState(this.IsDirty);
+        this.isConfirmDelete = false;
+        this.InvokeStateHasChanged();
+    }
+
+    private void FailedRoutingAttempt(object? sender, EventArgs e)
+        => this.SetMessage("You can't navigate away from this form with unsaved data", "alert-warning");
+
+    private void FailedExitAttempt(object? sender, EventArgs e)
+    => this.SetMessage("You attempted to exit the application", "alert-warning");
+
+    protected void ResetRecord()
+    {
+        this.Service.EditModel.Reset();
+        this.SetMessage("Fields reset to database values", "alert-info");
+    }
+
+    protected async Task<bool> SaveRecord()
+    {
+        var result = false;
+
+        if (!this.editContext.Validate())
+        {
+            this.SetMessage("There are validation problems", "alert-danger");
+            return false;
+        }
+
+        result = await this.Service.UpdateRecordAsync();
+        this.blazrNavManager?.SetLockState(this.IsDirty);
+
+        _ = result
+            ? this.SetMessage("Record Saved", "alert-success")
+            : this.SetMessage(this.Service.Message ?? "Problem saving record", "alert-danger");
+
+        return result;
+    }
+
+    protected virtual async Task SaveRecordAndExit()
+    {
+        if (await this.SaveRecord())
+            await DoExit();
+    }
+
+    protected virtual async Task<bool> AddRecord()
+    {
+        if (!this.editContext.Validate())
+        {
+            this.SetMessage("There are validation problems", "alert-danger");
+            return false;
+        }
+
+        var result = await this.Service.AddRecordAsync();
+        this.blazrNavManager?.SetLockState(this.IsDirty);
+
+        if (result)
+        {
+            this.SetMessage("Record Added", "alert-success");
+            await this.Service.LoadRecordAsync(this.Service.EditModel.Uid);
+            return true;
+        }
+
+        this.SetMessage(this.Service.Message ?? "Problem adding record", "alert-danger");
+        return false;
+    }
+
+    protected virtual async Task AddRecordAndExit()
+    {
+        if (await this.AddRecord())
+            await DoExit();
+    }
+
+    protected void ClearConfirmDelete()
+        => this.isConfirmDelete = false;
+
+    protected void ShowConfirmDelete()
+        => this.isConfirmDelete = true;
+
+    protected void OnRecordChanged(object? sender, EventArgs e)
+        => this.StateHasChanged();
+
+    protected async void ExitWithoutSaving()
+    {
+        this.blazrNavManager?.SetLockState(false);
+        await DoExit();
+    }
+
+    protected bool SetMessage(string message, string colour)
+    {
+        this.alertMessage = message;
+        this.alertColour = colour;
+        this.alertTimeOut = 0;
+        this.alertId = Guid.NewGuid();
+        this.StateHasChanged();
+        return true;
+    }
+
+    public virtual void Dispose()
+    {
+        if (this.editContext is not null)
+            this.editContext.OnFieldChanged -= FieldChanged;
+
+        if (this.blazrNavManager is not null)
+        {
+            this.blazrNavManager.BrowserExitAttempted -= FailedExitAttempt;
+            this.blazrNavManager.NavigationEventBlocked -= FailedRoutingAttempt;
+        }
+    }
+}
