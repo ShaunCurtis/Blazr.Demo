732928e4bce73fc6afabca279c9a6f273846bb76
Shaun Curtis <shaun@coldelm.com>
2022-11-05 21:11:02 +0000

    Update

--- Libraries/Blazr.UI/Components/NewInputControls/BlazrInputBase.cs
+++ Libraries/Blazr.UI/Components/NewInputControls/BlazrInputBase.cs
@@ -29,9 +29,9 @@ public class BlazrInputBase<TValue> : UIComponentBase
         => this.RecordEditContext?.HasMessages(this.FieldName) ?? false
         ? "is-invalid"
         : "is-valid";
-        
+
     protected string? ValueAsString
-        => GetValueAsString(this.Value);
+        => GetValueAsString(this.Value, this.Type);
 
     protected override ValueTask<bool> OnParametersChangedAsync(bool firstRender)
     {
@@ -59,7 +59,7 @@ public class BlazrInputBase<TValue> : UIComponentBase
             this.RecordEditContext.ValidationStateUpdated -= this.OnValidationStateUpdated;
     }
 
-    private static string? GetValueAsString(TValue? initialValue) =>
+    private static string? GetValueAsString(TValue? initialValue, string type) =>
         initialValue switch
         {
             string @value => value,
@@ -71,8 +71,24 @@ public class BlazrInputBase<TValue> : UIComponentBase
             decimal @value => BindConverter.FormatValue(value),
             TimeOnly @value => BindConverter.FormatValue(value, format: "HH:mm:ss"),
             DateOnly @value => BindConverter.FormatValue(value, format: "yyyy-MM-dd"),
-            DateTime @value => BindConverter.FormatValue(value, format: "yyyy-MM-ddTHH:mm:ss"),
-            DateTimeOffset @value => BindConverter.FormatValue(value, format: "yyyy-MM-ddTHH:mm:ss"),
+            DateTime @value => GetDateString(value, type),
+            DateTimeOffset @value => GetDateString(value, type),
             _ => initialValue?.ToString() ?? throw new InvalidOperationException($"Unsupported type {initialValue?.GetType()}")
+        };
+
+    private static string GetDateString(DateTime value, string type)
+        => type.ToLower() switch
+        {
+            "date" => BindConverter.FormatValue(value, format: "yyyy-MM-dd"),
+            "time" => BindConverter.FormatValue(value, format: "HH:mm:ss"),
+            _ => BindConverter.FormatValue(value, format: "yyyy-MM-ddTHH:mm:ss")
+        };
+
+    private static string GetDateString(DateTimeOffset value, string type)
+        => type.ToLower() switch
+        {
+            "date" => BindConverter.FormatValue(value, format: "yyyy-MM-dd"),
+            "time" => BindConverter.FormatValue(value, format: "HH:mm:ss"),
+            _ => BindConverter.FormatValue(value, format: "yyyy-MM-ddTHH:mm:ss")
         };
 }
--- ProjectLibraries/Blazr.App.Core/Entities/WeatherForecast/Services/WeatherForecastServiceCollection.cs
+++ ProjectLibraries/Blazr.App.Core/Entities/WeatherForecast/Services/WeatherForecastServiceCollection.cs
@@ -15,6 +15,7 @@ public static class WeatherForecastServiceCollection
         services.AddScoped<IReadService<DvoWeatherForecast, WeatherForecastEntity>, StandardReadService<DvoWeatherForecast, WeatherForecastEntity>>();
         services.AddScoped<IReadService<DboWeatherForecast, WeatherForecastEntity>, StandardReadService<DboWeatherForecast, WeatherForecastEntity>>();
         services.AddScoped<IEditService<DboWeatherForecast, DeoWeatherForecast, WeatherForecastEntity>, StandardEditService<DboWeatherForecast, DeoWeatherForecast, WeatherForecastEntity>>();
+        services.AddScoped<IContextEditService<WeatherForecastEditContext, DboWeatherForecast>, StandardEditContextService<WeatherForecastEditContext, DboWeatherForecast, WeatherForecastEntity>>();
         services.AddScoped<IEntityService<WeatherForecastEntity>, WeatherForecastService>();
     }
 }
