c31e8ddd37562a38089db4b62b9f75ab403f21ed
Shaun Curtis <shaun@coldelm.com>
2024-07-06 20:50:00 +0100

    Add Vanilla

--- Source/Applications/Blazr.Weather/Blazr.Weather.AppHost/Program.cs
+++ Source/Applications/Blazr.Weather/Blazr.Weather.AppHost/Program.cs
@@ -6,4 +6,6 @@ builder.AddProject<Projects.Blazr_App_Fluent_WASM_Server>("blazr-app-fluent-wasm
 
 builder.AddProject<Projects.Blazr_App_MudBlazor_Server>("blazr-app-mudblazor-server");
 
+builder.AddProject<Projects.Blazr_App_Vanilla_Server>("blazr-app-vanilla-server");
+
 builder.Build().Run();
