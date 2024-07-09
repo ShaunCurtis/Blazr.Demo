65b5239fd5e20557abfa31bad30e801022cfc882
Shaun Curtis <shaun@coldelm.com>
2024-07-09 09:14:55 +0100

    Update

--- Source/Applications/Blazr.Weather/Blazr.Weather.AppHost/Program.cs
+++ Source/Applications/Blazr.Weather/Blazr.Weather.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Blazr_App_Fluent_Server>("blazr-app-fluent-server");
 
-builder.AddProject<Projects.Blazr_App_Fluent_WASM_Server>("blazr-app-fluent-wasm-server");
+//builder.AddProject<Projects.Blazr_App_Fluent_WASM_Server>("blazr-app-fluent-wasm-server");
 
 builder.AddProject<Projects.Blazr_App_MudBlazor_Server>("blazr-app-mudblazor-server");
 
