8e661a86ac50c842b4b1472ef521637424f8d6ab
Shaun Curtis <shaun@coldelm.com>
2024-08-10 11:41:42 +0100

    Update

---
+++ Source/Applications/Blazr.Weather/App/Blazr.App.UI.Mud/WeatherForecasts/Forms/WeatherForecastEditContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+using Blazr.App.Core;
+
+namespace Blazr.App.UI.Mud;
+
+public static class WeatherForecastEditContextExtensions
+{
+    /// <summary>
+    /// Provides specific state on Summry for the FluentUI autocomplete control
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public static bool IsSummaryClean(this WeatherForecastEditContext context) => context.Summary is not null ? context.Summary.Equals(context.BaseRecord.Summary) : true;
+}
