1cc9f45a74f95d60eece1611cb76c7e31f10ac68
Shaun Curtis <shaun@coldelm.com>
2024-09-26 20:06:43 +0100

    Update - changes to Blazr.Diode

--- Source/Applications/Blazr.Invoicing/App/Blazr.App.Infrastructure/Usings.cs
+++ Source/Applications/Blazr.Invoicing/App/Blazr.App.Infrastructure/Usings.cs
@@ -1,7 +1,7 @@
 ﻿global using Blazr.App.Core;
 global using Blazr.FluxGate;
-global using Blazr.OneWayStreet.Core;
-global using Blazr.OneWayStreet.Infrastructure;
+global using Blazr.Diode.Core;
+global using Blazr.Diode.Infrastructure;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Logging;
 global using System.ComponentModel.DataAnnotations;
--- Source/Applications/Blazr.Weather/App/Blazr.App.UI.FluentUI/Components/Markdown/IStaticAssetService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Blazr.App.FluentUI.Components;
-
-public interface IStaticAssetService
-{
-    public Task<string?> GetAsync(string assetUrl, bool useCache = true);
-}
